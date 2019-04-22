e2a838eca443a02fd7d212ca660a45bb9256d2c6
Ashish Shetty aka AlphaSierra <30448930+Shetty073@users.noreply.github.com>
2019-04-22 16:38:40 +0530

    first upload

---
+++ js/clock.js
@@ -0,0 +1,33 @@
+function getClock() {
+    var date = new Date();
+
+    var day = date.getDay();
+    var hours = date.getHours();
+    var ampm = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    hours += "";
+    var minutes = date.getMinutes() + "";
+    var seconds = date.getSeconds() + "";
+
+    var weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    if (hours.length < 2) {
+        hours = "0" + hours;
+    }
+
+    if (minutes.length < 2) {
+        minutes = "0" + minutes;
+    }
+
+    if (seconds.length < 2) {
+        seconds = "0" + seconds;
+    }
+
+    var clock = weekDays[day] + " " + hours + ":" + minutes + ":" + seconds + " " + ampm;
+
+    document.getElementById("clock").innerHTML = clock;
+}
+
+getClock();
+setInterval(getClock, 1000);
